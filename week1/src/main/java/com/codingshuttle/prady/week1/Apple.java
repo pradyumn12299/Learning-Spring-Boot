6b35ff04892867b476be953dd4872f7acc12b835
pradyumn12299 <pradyumn1276@gmail.com>
2024-08-22 14:02:07 +0530

    entering week 1 progreass to git

---
+++ week1/src/main/java/com/codingshuttle/prady/week1/Apple.java
@@ -0,0 +1,10 @@
+package com.codingshuttle.prady.week1;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Apple {
+    void eatApple(){
+        System.out.printf("Iam eating the apple");
+    }
+}
---
+++ week1/src/main/java/com/codingshuttle/prady/week1/Week1Application.java
@@ -0,0 +1,26 @@
+package com.codingshuttle.prady.week1;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Week1Application implements CommandLineRunner {
+
+	@Autowired
+	Apple obj;
+
+
+	public static void main(String[] args) {
+		SpringApplication.run(Week1Application.class, args);
+	}
+
+
+	@Override
+	public void run(String... args) throws Exception {
+		obj.eatApple();
+	}
+
+
+}
